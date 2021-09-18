9523fa9498fd8ed58b2b874c37b54bbbc3d6ece9
kirill_maltsev <kirill_maltsev@outlook.com>
2021-09-18 15:23:24 +0300

    comit:)

---
+++ my turtel.py
@@ -0,0 +1,17 @@
+from turtle import *
+import time
+
+screen = Screen()
+screen.screensize(1000,1000)
+
+
+forward(250)
+right(90)
+forward(250)
+right(90)
+forward(250)
+right(90)
+forward(250)
+right(90/2+90)
+forward(250*1.41)
+mainloop()
---
+++ test.py
@@ -0,0 +1,3 @@
+print('heloo')
+print('2+2=4')
+print('2'+'2')
