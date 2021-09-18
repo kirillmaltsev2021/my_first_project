89b6c0b76db15517417e38797e085e3d62edbc81
kirill_maltsev <kirill_maltsev@outlook.com>
2021-09-18 16:11:35 +0300

    small square inside a big

--- my turtel.py
+++ my turtel.py
@@ -4,14 +4,12 @@ import time
 screen = Screen()
 screen.screensize(1000,1000)
 
-
-forward(250)
-right(90)
-forward(250)
-right(90)
-forward(250)
-right(90)
-forward(250)
-right(90/2+90)
+for i in range(8):
+    forward(50)
+    right(90)
+for i in range(4):
+    forward(250)
+    right(90)
+right(90/2)
 forward(250*1.41)
 mainloop()
